834c5c2efb9a450350d9025910341c60907fca92
amorcage <amorcage@outlook.com>
2024-02-13 17:11:22 +0100

    Affichage des jours restants (bleu/blanc/rouge) Todo : Mise en page

--- tempo.py
+++ tempo.py
@@ -8,7 +8,7 @@ import datetime
 intents = discord.Intents.default()
 intents.message_content = True  # Pour les événements de messages
 
-TOKEN = 'VOTRE_TOKEN_ICI'
+TOKEN = 'MTIwMzgzMjIyODY1Mjc4MTU4OA.G3MJJ8.oizuGS2jpZt4PtgIVCfj_OT7WGoGOeSWfPK7NA'
 CHANNEL_ID_TEMPO = '1206593461382553633'  # ID du salon pour la couleur du jour
 CHANNEL_ID_LEND = '1206870641316339712'  # ID du salon pour la couleur du lendemain
 
@@ -79,6 +79,24 @@ async def get_tempo_colors(ctx):
             
             await ctx.send(f"La couleur Tempo du jour ({current_date}) est :", embed=discord.Embed(color=current_day_discord_color, description=f"**{current_day_color_text.upper()}**"))
             await ctx.send(f"La couleur Tempo du lendemain ({tomorrow_date}) est :", embed=discord.Embed(color=tomorrow_discord_color, description=f"**{tomorrow_color_text.upper()}**"))
+
+            # Extraire le nombre de jours restants pour chaque couleur
+            days_remaining_element = soup.find('td', colspan='2')
+            if days_remaining_element:
+                strong_elements = days_remaining_element.find_all('strong')
+                if len(strong_elements) == 3:
+                    days_info = {
+                        'bleus': strong_elements[0].get_text(strip=True),
+                        'blancs': strong_elements[1].get_text(strip=True),
+                        'rouges': strong_elements[2].get_text(strip=True),
+                    }
+                    await ctx.send(f"Jours bleus restants : {days_info['bleus']}")
+                    await ctx.send(f"Jours blancs restants : {days_info['blancs']}")
+                    await ctx.send(f"Jours rouges restants : {days_info['rouges']}")
+                else:
+                    await ctx.send("Nombre incorrect de balises <strong> trouvées dans <td colspan='2'>.")
+            else:
+                await ctx.send("Balise <td colspan='2'> non trouvée.")
         else:
             await ctx.send("Impossible de trouver la couleur du jour ou du lendemain Tempo.")
     else:
