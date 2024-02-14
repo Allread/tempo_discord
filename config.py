1a0431c7ebe1c93b3d26f2839ddabf6d09490ede
amorcage <amorcage@outlook.com>
2024-02-14 13:30:44 +0100

    refacto

--- config.py
+++
@@ -1,6 +0,0 @@
-# TOKEN DU BOT DISCORD 
-TOKEN = ''
-
-# ID DES CHANNELS
-CHANNEL_ID_TEMPO = '1206593461382553633'  # ID du salon pour la couleur du jour
-CHANNEL_ID_LEND = '1206870641316339712'  # ID du salon pour la couleur du lendemain
